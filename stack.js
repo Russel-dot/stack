0d01d0b9b33dd5ed026e7eeb779b952d0def43f4
Russel-dot <70375771+Russel-dot@users.noreply.github.com>
2021-10-20 11:35:58 +0100

    Add files via upload

---
+++ stack.js
@@ -0,0 +1,17 @@
+ stack = [];
+
+function push(item){
+ stack.push(item);
+ console.log(stack)
+ }
+ function  pop();{
+     len =stack.length;
+     if(len >=1); {
+        stack.splice(len -1);
+      }
+     console.log(stack)
+ }
+ push(1)
+ push(2)
+ push(3)
+push(4)
